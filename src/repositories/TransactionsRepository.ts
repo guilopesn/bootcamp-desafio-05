02862e70de36e5ccbf60409a428b65b30732068e
Guilherme Nogueira <guilherme.nogueira@centralit.com.br>
2020-11-07 19:04:00 -0300

    Implementando funcionalidades requeridas

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+    let total = 0;
+
+    this.transactions.map(transaction => {
+      switch(transaction.type) {
+        case 'income':
+          income += transaction.value;
+          total += transaction.value;
+          break;
+        case 'outcome':
+          outcome += transaction.value;
+          total -= transaction.value;
+          break;
+      }
+    });
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
